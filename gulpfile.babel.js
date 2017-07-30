1db244ed1dba240a985718ee3101976ce5af3dd3
Ian Rose <ian@ianrose.me>
2017-07-29 21:13:13 -0400

    standard js, sourcemaps, sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,65 +1,74 @@
-import gulp from "gulp";
-import cp from "child_process";
-import gutil from "gulp-util";
-import postcss from "gulp-postcss";
-import cssImport from "postcss-import";
-import cssnext from "postcss-cssnext";
-import BrowserSync from "browser-sync";
-import webpack from "webpack";
-import webpackConfig from "./webpack.conf";
+import gulp from 'gulp'
+import cp from 'child_process'
+import gutil from 'gulp-util'
+import cssnano from 'gulp-cssnano'
+import sass from 'gulp-sass'
+import sourcemaps from 'gulp-sourcemaps'
+import BrowserSync from 'browser-sync'
+import webpack from 'webpack'
+import webpackConfig from './webpack.conf'
 
-const browserSync = BrowserSync.create();
-const hugoBin = "hugo";
-const defaultArgs = ["-d", "../dist", "-s", "site", "-v"];
+const browserSync = BrowserSync.create()
+const hugoBin = 'hugo'
+const defaultArgs = ['-d', '../dist', '-s', 'site', '-v']
 
-gulp.task("hugo", (cb) => buildSite(cb));
-gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
+gulp.task('hugo', (cb) => buildSite(cb))
+gulp.task('hugo-preview', (cb) => buildSite(cb, ['--buildDrafts', '--buildFuture']))
 
-gulp.task("build", ["css", "js", "hugo"]);
-gulp.task("build-preview", ["css", "js", "hugo-preview"]);
+gulp.task('build', ['styles', 'scripts', 'hugo'])
+gulp.task('build-preview', ['styles', 'scripts', 'hugo-preview'])
 
-gulp.task("css", () => (
-  gulp.src("./src/css/*.css")
-    .pipe(postcss([cssImport({from: "./src/css/main.css"}), cssnext()]))
-    .pipe(gulp.dest("./dist/css"))
+gulp.task('styles', () => (
+  gulp.src('./src/styles/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano({safe: true,
+      discardComments: false,
+      autoprefixer: {
+        browsers: 'last 5 version',
+        add: true
+      }
+    }))
+    .pipe(sourcemaps.write('.', {includeContents: false}))
+    .pipe(gulp.dest('./dist/styles'))
     .pipe(browserSync.stream())
-));
+))
 
-gulp.task("js", (cb) => {
-  const myConfig = Object.assign({}, webpackConfig);
+gulp.task('scripts', (cb) => {
+  const myConfig = Object.assign({}, webpackConfig)
 
   webpack(myConfig, (err, stats) => {
-    if (err) throw new gutil.PluginError("webpack", err);
-    gutil.log("[webpack]", stats.toString({
+    if (err) throw new gutil.PluginError('webpack', err)
+    gutil.log('[webpack]', stats.toString({
       colors: true,
       progress: true
-    }));
-    browserSync.reload();
-    cb();
-  });
-});
+    }))
+    browserSync.reload()
+    cb()
+  })
+})
 
-gulp.task("server", ["hugo", "css", "js"], () => {
+gulp.task('server', ['hugo', 'styles', 'scripts'], () => {
   browserSync.init({
     server: {
-      baseDir: "./dist"
+      baseDir: './dist'
     }
-  });
-  gulp.watch("./src/js/**/*.js", ["js"]);
-  gulp.watch("./src/css/**/*.css", ["css"]);
-  gulp.watch("./site/**/*", ["hugo"]);
-});
+  })
+  gulp.watch('./src/scripts/**/*.js', ['scripts'])
+  gulp.watch('./src/styles/**/*.scss', ['styles'])
+  gulp.watch('./site/**/*', ['hugo'])
+})
 
-function buildSite(cb, options) {
-  const args = options ? defaultArgs.concat(options) : defaultArgs;
+function buildSite (cb, options) {
+  const args = options ? defaultArgs.concat(options) : defaultArgs
 
-  return cp.spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
+  return cp.spawn(hugoBin, args, {stdio: 'inherit'}).on('close', (code) => {
     if (code === 0) {
-      browserSync.reload();
-      cb();
+      browserSync.reload()
+      cb()
     } else {
-      browserSync.notify("Hugo build failed :(");
-      cb("Hugo build failed");
+      browserSync.notify('Hugo build failed :(')
+      cb('Hugo build failed')
     }
-  });
+  })
 }
--- webpack.conf.js
+++ webpack.conf.js
@@ -1,16 +1,17 @@
-import webpack from "webpack";
-import path from "path";
+import webpack from 'webpack'
+import UglifyJsPlugin from 'webpack/lib/optimize/UglifyJsPlugin.js'
+import path from 'path'
 
 export default {
   module: {
     rules: [
       {
         test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader?name=/[hash].[ext]"
+        loader: 'file-loader?name=/[hash].[ext]'
       },
-      {test: /\.json$/, loader: "json-loader"},
+      {test: /\.json$/, loader: 'json-loader'},
       {
-        loader: "babel-loader",
+        loader: 'babel-loader',
         test: /\.js?$/,
         exclude: /node_modules/,
         query: {cacheDirectory: true}
@@ -18,20 +19,28 @@ export default {
     ]
   },
 
+  devtool: '#source-map',
+
   plugins: [
     new webpack.ProvidePlugin({
-      "fetch": "imports-loader?this=>global!exports?global.fetch!whatwg-fetch"
+      'fetch': 'imports-loader?this=>global!exports?global.fetch!whatwg-fetch'
+    }),
+    new UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
     })
   ],
 
-  context: path.join(__dirname, "src"),
+  context: path.join(__dirname, 'src'),
   entry: {
-    app: ["./js/app"]
+    app: ['./scripts/app']
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "[name].js"
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js'
   },
-  externals:  [/^vendor\/.+\.js$/]
-};
+  externals: [/^vendor\/.+\.js$/]
+}
